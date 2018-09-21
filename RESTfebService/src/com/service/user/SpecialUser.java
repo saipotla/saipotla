44cc2d04e7f224e37acfbf2ca9c0687c1d70e313
potla <potla@10.0.0.62>
2018-09-21 06:48:24 -0400

    initial change

---
+++ RESTfebService/src/com/service/user/SpecialUser.java
@@ -0,0 +1,17 @@
+package com.service.user;
+import javax.ws.rs.*;
+
+public class SpecialUser {
+
+	@PUT
+	public void createSpecialUser(){
+		
+		
+		
+	}
+	@GET
+	public void getSpecialUser(){
+		System.out.println("it is a very special");
+		
+	}
+}
